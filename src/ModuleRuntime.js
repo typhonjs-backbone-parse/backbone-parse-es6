19fa402a5c8e1e21a78d65b07b2d7d918000b969
typhonrt <support@typhonrt.org>
2015-12-22 03:00:36 -0800

    Integrated typhonjs-core-backbone-events

--- src/ModuleRuntime.js
+++ src/ModuleRuntime.js
@@ -6,7 +6,7 @@
 
 import Backbone         from 'backbone-es6/src/Backbone.js';
 import ParseCollection  from './ParseCollection.js';
-import TyphonEvents     from 'typhonjs-core-backbone-common/src/TyphonEvents.js';
+import TyphonEvents     from 'typhonjs-core-backbone-events/src/TyphonEvents.js';
 import History          from 'backbone-es6/src/History.js';
 import Model            from './ParseModel.js';
 import Router           from 'backbone-es6/src/Router.js';
